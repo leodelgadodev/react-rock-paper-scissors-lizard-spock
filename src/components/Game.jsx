4b2ed4a21425c0fd571f4951278018cbe06bcd27
leodelgadodev <leodelgado.dev@gmail.com>
2020-07-19 17:58:00 -0300

    game devuelve ganador entre dos jugadores humanos

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -12,27 +12,76 @@ import GameEngine from '../model/GameEngine';
 export default function Game() {
 
     const [playerTurn, setPlayerTurn] = useState("");
+    const [turn, setTurn] = useState(1);
+    const [turnCount, setTurnCount] = useState(1);
+    const [computerTurn, setComputerTurn] = useState(false);
 
     useEffect(() => {
-        setPlayerTurn(GameEngine.getCurrentPlayer());
-        console.log('turno de ' + playerTurn);
-    }, [playerTurn]);
+        if(turn === 1) {
+            setPlayerTurn(GameEngine.getPlayer(1));
+        } else {
+            setPlayerTurn(GameEngine.getPlayer(2));
+        }
+    }, []);
+
 
     const togglePlay = (choice) => {
-        GameEngine.togglePlay(choice);
+        // debugger; let player2 = GameEngine.getPlayer(2);
+        // debugger; setPlayerTurn(player2);
+        switch(turn) {
+            case 1: {
+                GameEngine.saveChoice(1, choice);
+                setPlayerTurn(GameEngine.getPlayer(2));
+                setTurn(2);
+                
+                if(GameEngine.getPlayer(2) === "Computador") {
+                    setComputerTurn(true);
+                    simulateComputerPlay();
+                }
+
+            } break; 
+            case 2: {
+                debugger; GameEngine.saveChoice(2, choice);
+                setPlayerTurn(GameEngine.getPlayer(1));
+                setTurn(1);
+            } break;
+        }
+        setTurnCount(turnCount+1);
+        console.log(playerTurn, turn, turnCount);
+
+        if(turnCount === 2) {
+            setTurnCount(1);
+            debugger; console.log(GameEngine.calculateRound());
+        }
+    }
+
+    const simulateComputerPlay = () => {
+        console.log(playerTurn);
+        setTimeout(() => {
+            setComputerTurn(false);
+            setTurn(1);
+            setPlayerTurn(GameEngine.getPlayer(1));
+        }, 1000);
     }
 
     return(
         <div className="max-w-xl rounded overflow-hidden shadow-md bg-white">
             <div className="font-bold text-xl text-center mt-2">Turno del {playerTurn}</div>
             <div className="px-12 py-8">
-                <div className="flex justify-center">
-                    <img className="h-16 w-16 rounded-full game-elem mx-2" src={rock} onClick={() => togglePlay("rock")}/>
-                    <img className="h-16 w-16 rounded-full game-elem mx-2" src={paper} onClick={() => togglePlay("paper")}/>
-                    <img className="h-16 w-16 rounded-full game-elem mx-2" src={scissors} onClick={() => togglePlay("scissors")}/>
-                    <img className="h-16 w-16 rounded-full game-elem mx-2" src={lizard} onClick={() => togglePlay("lizard")}/>
-                    <img className="h-16 w-16 rounded-full game-elem mx-2" src={spock} onClick={() => togglePlay("spock")}/>
-                </div>
+                    { 
+                        computerTurn ?
+                        <div className="flex justify-center">
+                            <span><p>El computador está pensando. Aguarde un momento por favor...</p></span> 
+                        </div>
+                        :
+                        <div className="flex justify-center">
+                            <img className="h-16 w-16 rounded-full game-elem mx-2" alt="Piedra" src={rock} onClick={() => togglePlay("rock")}/>
+                            <img className="h-16 w-16 rounded-full game-elem mx-2" alt="Papel" src={paper} onClick={() => togglePlay("paper")}/>
+                            <img className="h-16 w-16 rounded-full game-elem mx-2" alt="Tijeras" src={scissors} onClick={() => togglePlay("scissors")}/>
+                            <img className="h-16 w-16 rounded-full game-elem mx-2" alt="Lagarto" src={lizard} onClick={() => togglePlay("lizard")}/>
+                            <img className="h-16 w-16 rounded-full game-elem mx-2" alt="Spock" src={spock} onClick={() => togglePlay("spock")}/>
+                        </div>
+                    }
             </div>
         </div>
     );
--- src/model/GameEngine.js
+++ src/model/GameEngine.js
@@ -18,22 +18,38 @@ const GameEngine = {
         return State.currentPlayer;
     },
 
-    togglePlay(election) {
-        switch(State.currentPlayer) {
-            case "player1": {
-                State.currentPlayer = State.player2;
-                State.election1 = election;
-                console.log(State.election1);
+    getPlayer(number) {
+        let res;
+        switch(number) {
+            case 1: {
+                res = State.player1;
             } break;
-            case "player2": {
-                State.currentPlayer = State.player1;
-                State.election2 = election;
-                console.log(State.election2);
+            case 2: {
+                res = State.player2;
             }
         }
-        console.log(State.currentPlayer);
-        this.calculateRound();
+        return res;
     },
+
+    // togglePlay(election) {
+    //     console.log(election);
+    //     let currentPlayer = State.currentPlayer;
+    //     switch(currentPlayer) {
+    //         case "Jugador 1" || "Jugador": {
+    //             State.currentPlayer = State.player2;
+    //             State.election1 = election;
+    //             debugger; console.log(State.election1);
+    //         } break;
+    //         case "Jugador 2" || "Computador": {
+    //             State.currentPlayer = State.player1;
+    //             State.election2 = election;
+    //             debugger; console.log(State.election2);
+    //         }
+    //     }
+    //     currentPlayer = State.currentPlayer
+    //     debugger; console.log('turno de ' + currentPlayer);
+    //     this.calculateRound();
+    // },
 
     computerPlay() {
         let election = randomInt(1, 5);
@@ -56,55 +72,77 @@ const GameEngine = {
         }
     },
 
+    resetElections() {
+        State.election1 = "";
+        State.election2 = "";
+    },
+
     calculateRound() {
+        let res;
         let e1 = State.election1;
         let e2 = State.election2;
 
         switch(e1) {
             case "rock": {
                 if((e2 == "scissors") || (e2 == "lizard")) {
-                    State.score1++;
+                    res = {winner: State.player1, choice: State.election1};
                 } else {
-                    State.score2++;
+                    res = {winner: State.player2, choice: State.election2};
                 }
             } break;
             case "paper": {
                 if((e2 == "rock") || (e2 == "spock")) {
-                    State.score1++;
+                    res = {winner: State.player1, choice: State.election1};
                 } else {
-                    State.score2++;
+                    res = {winner: State.player2, choice: State.election2};
                 }
             } break;
             case "scissors": {
                 if((e2 == "paper") || (e2 == "lizard")) {
-                    State.score1++;
+                    res = {winner: State.player1, choice: State.election1};
                 } else {
-                    State.score2++;
+                    res = {winner: State.player2, choice: State.election2};
                 }
             } break;
             case "lizard": {
                 if((e2 == "paper") || (e2 == "spock")) {
-                    State.score1++;
+                    res = {winner: State.player1, choice: State.election1};
                 } else {
-                    State.score2++;
+                    res = {winner: State.player2, choice: State.election2};
                 }
             } break;
             case "spock": {
                 if((e2 == "scissors") || (e2 == "rock")) {
-                    State.score1++;
+                    res = {winner: State.player1, choice: State.election1};
                 } else {
-                    State.score2++;
+                    res = {winner: State.player2, choice: State.election2};
                 }
             } break;
+            default: {
+                res = {winner: false}
+            }
+
+            return res;
+        }
+
+        return res;
+    },
+
+    saveChoice(player, election) {
+        switch(player) {
+            case 1: State.election1 = election; break;
+            case 2: State.election2 = election; break;
         }
     },
 
     configureMode(mode) {
         switch(mode) {
             case "player": {
-               State.player2 = 'Jugador 2';
+                State.player1 = 'Jugador 1';
+                State.player2 = 'Jugador 2';
             } break;
             case "computer": {
+                State.player1 = 'Jugador';
                 State.player2 = "Computador"
             }
         }
@@ -113,6 +151,6 @@ const GameEngine = {
 
 function randomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
-  }
+}
 
 export default GameEngine;
